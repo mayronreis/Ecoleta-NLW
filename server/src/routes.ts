22162473bfc409044dff8182b06de996628593a5
mayron <mayronreislacerda@gmail.com>
2020-06-05 21:03:04 -0300

    Upload server

---
+++ server/src/routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+routes.post('/points', pointsController.create);
+
+
+
+
+export default routes;
